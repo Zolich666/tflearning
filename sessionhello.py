bbcd268b4028e2302b0fc934ccf14e97d715874d
Zolich666 <Licheng.Zong@outlook.com>
2019-08-26 11:39:03 +0800

    add annotations

--- sessionhello.py
+++ sessionhello.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-hello = tf.constant('Hello, Tensorflow!')
-sess = tf.Session()
-print(sess.run(hello))
-sess.close()
+hello = tf.constant('Hello, Tensorflow!')   #constant是tf 的常量
+sess = tf.Session()                         #建立session
+print(sess.run(hello))                      #用run来运行， 直接print(hello)是不行的
+#print(hello)
+sess.close()                                #关闭sess
